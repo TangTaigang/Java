53dd398e789dd31a0c9cb225315602bb9f87aad2
dominhquan <dominhquan.uit@gmail.com>
2014-08-23 21:07:52 +0700

    update

--- Console/src/main/java/hackerrank/LoveLetter.java
+++ Console/src/main/java/hackerrank/LoveLetter.java
@@ -6,18 +6,8 @@ public class LoveLetter {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] a=new int[5][];
 		
-		for(int i=0;i<5;i++){
-			for(int j=0;j<10;j++){
-				a[i][j]=j;
-			}
-		}
-		System.out.println(a.length);
-		for (int[] is : a) {
-			
-		}
-		
+	
 	}	
 
 }
